a51c29e076d86eff6ba6ed0e52fd8d3d5429ad00
Luyolo Mnganga <Luyolomnganga@gmail.com>
2022-10-18 12:21:02 +0200

    layouts

---
+++ resources/views/layouts/left_bar.blade.php
@@ -0,0 +1,79 @@
+
+<!-- left bar -->
+<div class="left-side-bar">
+    <div class="brand-logo">
+        <a class="navbar-brand" href="#">
+            <img src="vendors/images/logo.png" alt="logo" />
+          </a>
+        <div class="close-sidebar" data-toggle="left-sidebar-close">
+            <i class="ion-close-round"></i>
+        </div>
+    </div>
+    <div class="menu-block customscroll">
+        <div class="sidebar-menu">
+            <ul id="accordion-menu">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-house-1"></span><span class="mtext">Dashboard</span>
+                    </a>
+
+                </li>
+                <li>
+                    <a href="#" class="dropdown-toggle no-arrow">
+                        <span class="micon fa fa-calendar"></span><span class="mtext">Leave Planning</span>
+                    </a>
+
+
+                </li>
+
+                <li>
+                    <a href="#" class="dropdown-toggle no-arrow">
+                        <span class="micon fa fa-building"></span><span class="mtext">Department</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="dropdown-toggle no-arrow">
+                        <span class="micon"><i class="dw dw-user"></i></span><span class="mtext">Staff Leave Days</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="dropdown-toggle no-arrow">
+                        <span class="micon"><i class="material-icons">bed</i></span><span class="mtext">Leave Type</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="dropdown-toggle no-arrow">
+                        <span class="micon"><i class="material-icons">date_range</i></span><span class="mtext">Holidays</span>
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a ref="javascript:void(0);" class="dropdown-toggle">
+                        <span class="micon fa fa-users"></span><span class="mtext">Staff</span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="#">Manage Staff</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a href="javascript:void(0);" class="dropdown-toggle">
+                        <span class="micon dw dw-apartment"></span><span class="mtext"> Leave </span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="#">Apply Leave</a></li>
+
+                        <li><a href="#">Leave Plans</a></li>
+
+                        <li><a  href="#">Leaves</a></li>
+
+                        <li><a href="#">HOD Leaves</a></li>
+
+                        <li><a href="#">Department Leaves</a></li>
+
+                        <li><a href="#">My Leaves</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,37 @@
+<!-- nav bar -->
+<div class="header">
+    <div class="header-left">
+        <div class="menu-icon dw dw-menu"></div>
+        <div class="container-fluid systemname text-center" style="color: #C3bE5C;width:100%;">Vehicle Management System</div>    </div>
+    <div class="header-right">
+
+        <div class="dashboard-setting user-notification">
+            <div class="dropdown">
+                <a class="dropdown-toggle no-arrow" href="javascript:;" data-toggle="right-sidebar">
+                    <i class="dw dw-settings2"></i>
+                </a>
+            </div>
+        </div>
+
+
+        <div class="user-info-dropdown">
+            <div class="dropdown">
+                <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                    <span class="user-icon">
+                        <img src="vendors/images/photo5.jpg" alt="">
+                    </span>
+                    <span class="user-name">Admin</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                    <a class="dropdown-item" href ="#"><i class="dw dw-user1"></i> Profile</a>
+
+                    <a class="dropdown-item" href ="#"><i class="dw dw-file-3"></i> User Activity Feeds</a>
+                    <a class="dropdown-item" href ="#"><i class="dw dw-file-4"></i> Reports</a>
+
+                    <a class="dropdown-item" href ="#"><i class="dw dw-logout"></i> Log Out</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
