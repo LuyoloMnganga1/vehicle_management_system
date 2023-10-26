41c4a0bb3ff6309ee5f3e2178ac124698b4dee11
Luyolo Mnganga <Luyolomnganga@gmail.com>
2023-10-26 15:22:32 +0200

    Update calender.blade.php

--- resources/views/bookings/calender.blade.php
+++ resources/views/bookings/calender.blade.php
@@ -6,8 +6,6 @@ Booking Calender
 <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.css" rel="stylesheet" />
 <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.css" rel="stylesheet" />
 <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid/main.css" rel="stylesheet" />
-{{-- <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
-<link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'> --}}
 <style>
     .fc-day-today {
     background: #C3bE5C !important;
